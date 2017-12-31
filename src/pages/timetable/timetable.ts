405ea992976832b7eb8f385c31e8cfe620d64532
piskitus <marcalarcon1994@gmail.com>
2017-12-31 13:40:40 +0100

    timetable page demo done

--- src/pages/timetable/timetable.ts
+++ src/pages/timetable/timetable.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the TimetablePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -14,6 +8,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'timetable.html',
 })
 export class TimetablePage {
+
+  segment: string = "day1";//Segmento por defecto
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
